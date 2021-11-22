c8181c64a84abf8bb0440224ebf09420bba8d235
Moikapy <warrenckhan@gmail.com>
2021-11-22 12:36:14 -0500

    @moikapy remove build and revert build command to docs specs

--- build/web/_next/static/Na1OEKdwfab5kY0OuBuuu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-0b3991fcac05bbd29cc1.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

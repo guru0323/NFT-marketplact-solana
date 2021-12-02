88736d6834032840a22d8a905c55b51dc6d74cf7
moikapy <moikapy@devmoi.com>
2021-12-02 13:25:14 -0500

    @moikapy: add filter to listing to show live sales

--- packages/web/src/views/home/auctionList.tsx
+++ packages/web/src/views/home/auctionList.tsx
@@ -1,17 +1,17 @@
-import { LoadingOutlined } from '@ant-design/icons';
-import { useStore } from '@oyster/common';
-import { useWallet } from '@solana/wallet-adapter-react';
-import { Alert, Button, Spin } from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
+import {useStore} from '@oyster/common';
+import {useWallet} from '@solana/wallet-adapter-react';
+import {Alert, Button, Spin} from 'antd';
 import React from 'react';
 import useInfiniteScroll from 'react-infinite-scroll-hook';
-import { Link } from 'react-router-dom';
-import { AuctionRenderCard } from '../../components/AuctionRenderCard';
-import { MetaplexMasonry } from '../../components/MetaplexMasonry';
+import {Link} from 'react-router-dom';
+import {AuctionRenderCard} from '../../components/AuctionRenderCard';
+import {MetaplexMasonry} from '../../components/MetaplexMasonry';
 import {
   useAuctionManagersToCache,
   useInfiniteScrollAuctions,
 } from '../../hooks';
-import { Banner } from '../../components/Banner';
+import {Banner} from '../../components/Banner';
 
 export enum LiveAuctionViewState {
   All = '0',
@@ -21,8 +21,13 @@ export enum LiveAuctionViewState {
 }
 
 export const AuctionListView = () => {
-  const { auctions, loading, initLoading, hasNextPage, loadMore } =
-    useInfiniteScrollAuctions();
+  const {
+    auctions,
+    loading,
+    initLoading,
+    hasNextPage,
+    loadMore,
+  } = useInfiniteScrollAuctions();
 
   const [sentryRef] = useInfiniteScroll({
     loading,
@@ -31,34 +36,32 @@ export const AuctionListView = () => {
     rootMargin: '0px 0px 200px 0px',
   });
 
-  const { ownerAddress, storefront } = useStore();
+  const {ownerAddress, storefront} = useStore();
   const wallet = useWallet();
-  const { auctionManagerTotal, auctionCacheTotal } =
-    useAuctionManagersToCache();
+  const {auctionManagerTotal, auctionCacheTotal} = useAuctionManagersToCache();
   const isStoreOwner = ownerAddress === wallet.publicKey?.toBase58();
   const notAllAuctionsCached = auctionManagerTotal !== auctionCacheTotal;
   const showCacheAuctionsAlert = isStoreOwner && notAllAuctionsCached;
 
   return initLoading ? (
-    <div className="app-section--loading">
+    <div className='app-section--loading'>
       <Spin indicator={<LoadingOutlined />} />
     </div>
   ) : (
     <>
       {showCacheAuctionsAlert && (
         <Alert
-          message="Attention Store Owner"
-          className="app-alert-banner metaplex-spacing-bottom-lg"
+          message='Attention Store Owner'
+          className='app-alert-banner metaplex-spacing-bottom-lg'
           description={
             <p>
               Make your storefront faster by enabling listing caches.{' '}
               {auctionCacheTotal}/{auctionManagerTotal} of your listing have a
               cache account. Watch this{' '}
               <a
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://youtu.be/02V7F07DFbk"
-              >
+                rel='noopener noreferrer'
+                target='_blank'
+                href='https://youtu.be/02V7F07DFbk'>
                 video
               </a>{' '}
               for more details and a walkthrough. On November 17rd storefronts
@@ -66,30 +69,35 @@ export const AuctionListView = () => {
               are generating a cache account.
             </p>
           }
-          type="info"
+          type='info'
           showIcon
           action={
-            <Link to="/admin">
+            <Link to='/admin'>
               <Button>Visit Admin</Button>
             </Link>
           }
         />
       )}
-      { storefront.theme.banner && 
-        <Banner src={storefront.theme.banner} headingText={''} subHeadingText={''} />
-      }
+      {storefront.theme.banner && (
+        <Banner
+          src={storefront.theme.banner}
+          headingText={''}
+          subHeadingText={''}
+        />
+      )}
       <MetaplexMasonry>
         {auctions.map((m, idx) => {
           const id = m.auction.pubkey;
-          return (
-            <Link to={`/auction/${id}`} key={idx}>
-              <AuctionRenderCard key={id} auctionView={m} />
-            </Link>
-          );
+          if (m.auction.info.state !== 2)
+            return (
+              <Link to={`/auction/${id}`} key={idx}>
+                <AuctionRenderCard key={id} auctionView={m} />
+              </Link>
+            );
         })}
       </MetaplexMasonry>
       {hasNextPage && (
-        <div className="app-section--loading" ref={sentryRef}>
+        <div className='app-section--loading' ref={sentryRef}>
           <Spin indicator={<LoadingOutlined />} />
         </div>
       )}

0cf52f305827f3c4af17be05dec1aef4cd04436c
Phillip Strefling <phillip@strefling.pro>
2021-12-30 08:48:56 -0500

    fixed doc

--- app/packages/web/src/pages/_document.tsx
+++ app/packages/web/src/pages/_document.tsx
@@ -83,24 +83,3 @@ export default class MetaplexDocument extends Document {
     );
   }
 }
-
-class CheckoutDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          {/*Below we add the modal wrapper*/}
-          <div id="modal-root"></div>
-        </body>
-      </Html>
-    );
-  }
-}
